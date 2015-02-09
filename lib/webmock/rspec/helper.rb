625e0a6f7e60f05db90983126a75350193fe0eb1
Logan Serman <logan.serman@metova.com>
2015-02-09 13:19:33 -0600

    Drop 2.1.0 from Travis

--- lib/webmock/rspec/helper.rb
+++
@@ -1,9 +0,0 @@
-require "webmock/rspec/helper/version"
-
-module Webmock
-  module Rspec
-    module Helper
-      # Your code goes here...
-    end
-  end
-end
--- lib/webmock/rspec/helper/version.rb
+++
@@ -1,7 +0,0 @@
-module Webmock
-  module Rspec
-    module Helper
-      VERSION = "0.0.1"
-    end
-  end
-end
