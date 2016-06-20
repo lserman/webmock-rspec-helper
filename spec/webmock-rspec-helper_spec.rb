f7118f88c89c07187591e7a72cd444e851f1c706
Logan Serman <logan.serman@metova.com>
2016-06-20 11:28:23 -0500

    Tidy up code

--- spec/webmock-rspec-helper_spec.rb
+++ spec/webmock-rspec-helper_spec.rb
@@ -15,35 +15,35 @@ end
 
 describe '#webmock' do
   it 'mocks GET google using default response 200' do
-    webmock :get, %r[google.com] => 'GET_google.json'
-    response = GET('http://google.com')
+    webmock :get, %r{google.com} => 'GET_google.json'
+    response = GET 'http://google.com'
     expect(response.status).to eq 200
     expect(response.body['google']).to eq true
   end
 
   it 'mocks GET google with custom response code' do
-    webmock :get, %r[google.com] => 'GET_google.999.json'
-    response = GET('http://google.com')
+    webmock :get, %r{google.com} => 'GET_google.999.json'
+    response = GET 'http://google.com'
     expect(response.status).to eq 999
     expect(response.body['google']).to eq true
   end
 
   it 'accepts a block that returns the with options' do
-    webmock :get, %r[google.com] => 'GET_google.json', with: Hash[query: { test: '123' }]
-    expect { GET('http://google.com') }.to raise_error(WebMock::NetConnectNotAllowedError) rescue nil
-    response = GET('http://google.com?test=123')
+    webmock :get, %r{google.com} => 'GET_google.json', with: Hash[query: { test: '123' }]
+    expect { GET 'http://google.com' }.to raise_error WebMock::NetConnectNotAllowedError
+    response = GET 'http://google.com?test=123'
     expect(response.status).to eq 200
   end
 
   it 'mocks GET google with an empty body and given status code' do
-    webmock :get, %r[google.com] => 204
-    response = GET('http://google.com')
+    webmock :get, %r{google.com} => 204
+    response = GET 'http://google.com'
     expect(response.body).to eq ''
     expect(response.status).to eq 204
   end
 
   it 'mocks GET google with response headers' do
-    webmock :get, %r[google.com] => 'GET_google.json', headers: { 'Content-Type' => 'text/html' }
+    webmock :get, %r{google.com} => 'GET_google.json', headers: { 'Content-Type' => 'text/html' }
     response = GET 'http://google.com'
     expect(response.content_type).to eq 'text/html'
   end
